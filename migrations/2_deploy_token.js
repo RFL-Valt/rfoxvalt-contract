8f60ed4cd05ac69859dfb292fe7ff770baa1a6b6
Huy Pham <huy.pham@kpis.com.vn>
2021-09-10 17:18:15 +0700

    update deploy file

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,6 +1,6 @@
 const { encodeCall } = require("@openzeppelin/upgrades");
 
-const MockBWP = artifacts.require("MockBWP.sol");
+//const MockBWP = artifacts.require("MockBWP.sol");
 const BWPNFT = artifacts.require("BWPNFT.sol");
 const BWPAuction = artifacts.require("BWPAuction.sol");
 
@@ -12,13 +12,14 @@ module.exports = async (deployer, network, addresses) => {
     proxyRegistryAddress = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
   }
 
-  await deployer.deploy(MockBWP, "BWP Token", "BWP");
+  //await deployer.deploy(MockBWP, "BWP Token", "BWP");
   await deployer.deploy(BWPNFT, proxyRegistryAddress);
-  const bwpAddress = MockBWP.address;
+  const rfoxAddress = "0xa1d6df714f91debf4e0802a542e13067f31b8262";
+  //const bwpAddress = MockBWP.address;
   const bidPricePercent = 103;
-  await deployer.deploy(BWPAuction, bwpAddress, bidPricePercent);
+  await deployer.deploy(BWPAuction, rfoxAddress, bidPricePercent);
 
   console.log("BWPNFT", BWPNFT.address);
-  console.log("MockBWP", MockBWP.address);
+  //console.log("MockBWP", MockBWP.address);
   console.log("BWPAuction", BWPAuction.address);
 };
